f625651140b85d1d97dc7de93d22cf8c0d26126e
roadpilot <mdacht777@gmail.com>
2021-11-17 14:25:52 -0700

    Test demo

--- Demo.java
+++ Demo.java
@@ -1,9 +1,5 @@
 public class Demo {
-    public static String mSmall = "Men's Small";
-    public static String mMedium = "Men's Medium";
-    public static String mLarge = "Men's Large";
-
-    public static String listAllSizes() {
-        return mSmall + ", " + mMedium + ", " + mLarge;
+    public static void main(String[] args){
+        System.out.println(ItemSizes.listAllSizes());
     }
 }
---
+++ ItemSizes.java
@@ -0,0 +1,9 @@
+public class ItemSizes {
+    public static String mSmall = "Men's Small";
+    public static String mMedium = "Men's Medium";
+    public static String mLarge = "Men's Large";
+
+    public static String listAllSizes() {
+        return mSmall + ", " + mMedium + ", " + mLarge;
+    }
+}
