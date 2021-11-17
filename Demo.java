1b185aeda3a053183ed74c041f82147a2167c78d
roadpilot <mdacht777@gmail.com>
2021-11-17 14:28:20 -0700

    listAllSizes

--- Demo.java
+++ Demo.java
@@ -1,5 +1,12 @@
 public class Demo {
     public static void main(String[] args){
         System.out.println(ItemSizes.listAllSizes());
+
+        Item item = new Item();
+        item.setSize(ItemSizes.mSmall);
+        System.out.println(item.size);
+
+        item.setSize(ItemSizes.mLarge);
+        System.out.println(item.size);
     }
 }
