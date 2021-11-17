214bed220f130ba1a82d10cc70b379c1028b3980
roadpilot <mdacht777@gmail.com>
2021-11-17 14:20:20 -0700

    Demo class stub

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static String mSmall = "Men's Small";
+    public static String mMedium = "Men's Medium";
+    public static String mLarge = "Men's Large";
+
+    public static String listAllSizes() {
+        return mSmall + ", " + mMedium + ", " + mLarge;
+    }
+}
