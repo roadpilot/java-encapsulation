94f17386eac70b8abeed0fc64d9c32d57821788e
roadpilot <mdacht777@gmail.com>
2021-11-18 14:45:14 -0700

    demo as instance variables

--- Demo.java
+++ Demo.java
@@ -1,12 +1,22 @@
 public class Demo {
-    public static void main(String[] args){
-        System.out.println(ItemSizes.listAllSizes());
+    // public static void main(String[] args){
+    //     System.out.println(ItemSizes.listAllSizes());
 
-        Item item = new Item();
-        item.setSize(ItemSizes.mSmall);
-        System.out.println(item.size);
+    //     Item item = new Item();
+    //     item.setSize(ItemSizes.mSmall);
+    //     System.out.println(item.size);
 
-        item.setSize(ItemSizes.mLarge);
-        System.out.println(item.size);
+    //     item.setSize(ItemSizes.mLarge);
+    //     System.out.println(item.size);
+    // }
+    public static void main(String[] args) {
+        ExampleConstants constx = new ExampleConstants();
+        System.out.println(constx.COLUMNS);
+        System.out.println(constx.TIMEOUT);
+        System.out.println(constx.MAX_CONNECTIONS);
+        System.out.println(constx.PROTOCOL);
+        System.out.println(constx.METHOD);
+        
     }
+
 }
---
+++ ExampleConstants.java
@@ -0,0 +1,7 @@
+public class ExampleConstants {
+    final int COLUMNS = 80;
+    final int TIMEOUT = 60;
+    final int MAX_CONNECTIONS = 1024;
+    final String PROTOCOL = "HTTP";
+    final String METHOD = "POST";
+}
