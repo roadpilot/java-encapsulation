51e07fc789be9c42368de5e6c8f994c4735de531
roadpilot <mdacht777@gmail.com>
2021-11-19 10:15:46 -0700

    demo with 2nd constructor

--- OverloadDemo.java
+++ OverloadDemo.java
@@ -1,6 +1,6 @@
 public class OverloadDemo {
     public static void main(String[] args) {
-        Shirt theShirt = new Shirt('R', 9.99);
+        Shirt theShirt = new Shirt('R', 9.99, "Nice shirt");
         theShirt.display();
     }
 }
