3c9057747cf5d5d35cb0c336a2f100231a34688c
roadpilot <mdacht777@gmail.com>
2021-11-19 07:30:56 -0700

    access control demo Item

--- Item.java
+++ Item.java
@@ -1,6 +1,18 @@
 public class Item {
-    public String size;
-    public void setSize(String sizeArg){
-        size = sizeArg;
+    private double price = 15.50;
+
+    public void setPrice(double price, Customer cust) {
+        this.price = price;
+        adjustPrice(cust);        
+    }
+
+    private void adjustPrice(Customer cust) {
+        if (cust.hasLoyaltyDiscount){
+            price = price * .85;
+        }
+    }
+
+    public getPrice() {
+        return String.format("$%.2f", price);        
     }
 }
